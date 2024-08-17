92d7bfc6d198c5d04c954a418f24973301147ba5
RAJWARDHAN-B <147420707+RAJWARDHAN-B@users.noreply.github.com>
2024-08-17 20:40:31 +0530

    some further changes

--- game.js
+++ game.js
@@ -1,3 +1,38 @@
 // console.log($("h1"));
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern=[];
 
-nextSequence()
+
+
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+
+});
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()* 4);
+    console.log(randomNumber);
+    gamePattern.push(randomNumber);
+    var randomChosenColour = buttonColours[randomNumber];
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name){
+    var audio = new Audio("sounds/"+name+".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+
+}
+
+
